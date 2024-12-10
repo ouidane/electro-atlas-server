13d12471d623c2b27cf92b3ec60e057f0b0d80ae
ouidane <ouidanezakaria@gmail.com>
2024-12-10 19:34:49 +0100

    capture raw body for Stripe webhook

--- src/middlewares/rawBodyMiddleware.ts
+++ src/middlewares/rawBodyMiddleware.ts
@@ -2,8 +2,8 @@ import express, { Request, Response, NextFunction } from "express";
 
 // Middleware to capture raw body for Stripe webhook
 const rawBodyMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (req.url.startsWith("/api/v1/payment/webhook")) {
-    next();
+  if (req.originalUrl === "/api/v1/payment/webhook") {
+    express.raw({ type: "application/json" })(req, res, next);
   } else {
     express.json()(req, res, next);
   }
